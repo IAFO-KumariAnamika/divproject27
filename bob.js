796f1681991faf899e084537b2bc77d1135baa34
IAFO-KumariAnamika <kumarianamika5798@gmail.com>
2021-01-22 22:50:18 +0530

    added radius

--- bob.js
+++ bob.js
@@ -1,13 +1,16 @@
 class Bob{
-    constructor(x,y){
+    constructor(x,y,){
         var options={
             isStatic:false,
             restitution:1,
             friction:0,
-            density:7.8
+            density:0.8
 
         }
-        this.body=Bodies.circle(x,y,25,options);
+        this.r=50;
+        this.x=x;
+        this.y=y;
+        this.body=Bodies.circle(this.x,this.y,(this.r)/2,options);
         World.add(world,this.body);
 
 
@@ -15,9 +18,11 @@ class Bob{
 
     display(){
         push();
-        ellipseMode(RADIUS);
+        translate(this.body.position.x,this.body.position.y);
+        ellipseMode(CENTER);
+        
         fill(254,0,255);
-        ellipse(this.body.position.x,this.body.position.y,25,25);
+        ellipse(0,0,this.r,this.r);
         pop();
     }
 }
